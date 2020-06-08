961e1c9292a694116ba1df92b60b2516ea31e00e
Phonesalo Sihanorak <psihanorak@gmail.com>
2020-06-08 11:12:10 -0500

    Added Play Files

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,11 @@
 import 'bootstrap';
 import '../styles/main.scss';
 import eat from './data/eat';
+import play from './data/play';
 
 const init = () => {
   eat.createEatQuadrant();
+  play.createPlayQuadrant();
 };
 
 init();
