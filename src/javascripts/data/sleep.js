afffc44ce19d35bf8ea3f45c4f058de8aa175cc7
Phonesalo Sihanorak <psihanorak@gmail.com>
2020-06-08 14:04:41 -0500

    Added Sleep Files

---
+++ src/javascripts/data/sleep.js
@@ -0,0 +1,31 @@
+import utils from '../utility/utils';
+
+let energy = 50;
+
+const createSleepQuadrant = () => {
+  let domString = '';
+
+  domString += `<div>
+                <h2>SLEEP</h2>
+                <h4>Energy status: ${energy}</h4>
+                <button id="nap"><i class="nap"></i></button>
+                <button id="slumber"><i class="slumber"></i></button>
+                </div>`;
+
+  utils.printToDom('#sleep', domString);
+};
+
+const nap = () => {
+  energy = Math.min(energy + 50, 100);
+  createSleepQuadrant();
+};
+
+const slumber = () => {
+  energy = Math.min(energy + 60, 100);
+  createSleepQuadrant();
+};
+
+$('body').on('click', '#nap', nap);
+$('body').on('click', '#slumber', slumber);
+
+export default { createSleepQuadrant };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,11 +3,13 @@ import '../styles/main.scss';
 import eat from './data/eat';
 import play from './data/play';
 import fight from './data/fight';
+import sleep from './data/sleep';
 
 const init = () => {
   eat.createEatQuadrant();
   play.createPlayQuadrant();
   fight.createFightQuadrant();
+  sleep.createSleepQuadrant();
 };
 
 init();
